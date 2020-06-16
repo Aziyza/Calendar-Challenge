00e9d10254f2bc5a286afe79292f7d26cfaa9ac6
Aziyza <aziza.ismailova.99@gmail.com>
2020-06-17 02:11:02 +0500

    ready for coding

---
+++ Calendar-Challenge/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Calendar-Challenge
+//
+//  Created by Mac on 6/17/20.
+//  Copyright © 2020 Mac. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
